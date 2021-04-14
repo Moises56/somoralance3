4d3fee6956bbd05b67252c00379290f8a0004bf2
Moises56 <moiavil56@gmail.com>
2021-04-13 18:17:00 -0600

    heroku Commit

--- backend/server/database.js
+++ backend/server/database.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/somoralancedb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+// mongoose.connect('mongodb://localhost/somoralancedb', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
 
+// })
+//     .then(db => console.log('base Conectada'))
+//     .catch(err => console.log(err));
+
+    mongoose.set('useFindAndModify', false);
+    mongoose.connect('mongodb+srv://moises:asd.456@cluster0.rucpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
 })
-    .then(db => console.log('base Conectada'))
-    .catch(err => console.log(err));
+ .then(db => console.log('Base Conectada'))
+ .catch(err => console.log(err));
+ 
+ //mongodb+srv://moises:asd.456@cluster0.rucpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-//Install express server
+//?Install express server
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-// const { dirname } = require('node:path');
+//* const { dirname } = require('node:path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 var app_path = './dist/angular-app-heroku';
 require('./backend/server/database');
 
-// app.use(express.static('../dist/angular-app-heroku'));
+//? app.use(express.static('../dist/angular-app-heroku'));
 
 app.use(cors());
 app.use(express.json()); //?para leer los datos quue se estan resiviendo
@@ -17,23 +17,23 @@ app.use(express.json()); //?para leer los datos quue se estan resiviendo
 app.use('/api', require('./backend/server/routes/index'))
 
 
-// Serve only the static files form the dist directory
+//* Serve only the static files form the dist directory
 
-// app.get('/*', (req, res) =>
-//     res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-// );
+//* app.get('/*', (req, res) =>
+//*     res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+//* );
 
 
 app.use('/', express.static(path.join(__dirname, app_path)))
 .get('*', (req, res) => res.sendFile(path.join(__dirname, app_path + '/index.html')))
 .listen(PORT,()=>console.log(`servidor en puerto ${PORT}`));
 
-// Start the app by listening on the default Heroku port
-// app.set('port', process.env.PORT || 8080);
-// app.set('port', process.env.PORT || 8080);
-// app.listen(process.env.PORT || 8080);
+//? Start the app by listening on the default Heroku port
+//? app.set('port', process.env.PORT || 8080);
+//? app.set('port', process.env.PORT || 8080);
+//? app.listen(process.env.PORT || 8080);
 
 
-// module.exports = app;
+//? module.exports = app;
 
 
